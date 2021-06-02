f1fc1aa0dfa413bf2faa439ed139e1c03ab1a50e
DorukAkinci <dakinci@meshcloud.io>
2021-06-02 15:04:05 +0200

    feat(vpc): adding new service

---
+++ network/create-vnets.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -o errexit  # exit on error
+set -o errtrace  # enables ERR traps so we can run cleanup
+set -o pipefail  # exit on error in a pipe, without this only the status of the last command in a pipe is considered
+set -o nounset  # exit on undefined variables
+
+shopt -s globstar
+
+for filepath in **/*.main.tf; do
+    [ -f "$filepath"  ] || continue
+
+    echo "executing $filepath"
+
+    filename=$(basename "$filepath")
+    instance_id="${filename%.main.tf}"
+
+    unipipe update --instance-id "$instance_id" --status "succeeded" --description "VNet Service Ready. To order a VNet, please add a binding." ./
+
+     # terraform -chdir="$(dirname "$filepath")" init
+     # terraform -chdir="$(dirname "$filepath")" plan
+done
---
+++ network/network-handlers.js
@@ -0,0 +1,64 @@
+
+class VnetHandler {
+  name = "Vnet Handler";
+
+  handle(service) {
+    const params = service.instance.parameters;
+    const context = service.instance.context;
+    const bindings = service.bindings;
+
+    // We read the gcp project id out of the first tenant binding we find.
+    // TODO Error handling for no or multiple bindings
+    const azure_subscription_id = bindings?.[0]?.binding?.bindResource?.tenant_id
+
+    return {
+      // Hierarchy level 1
+      name: "network",
+      entries: [{
+        // Hierarchy level 2: Folder "<customer id>"
+        name: context.customer_id,
+        entries: [
+          {
+            // Hierarchy level 3: Folder "<project id>"
+            name: context.project_id,
+            entries: [
+              {
+                // Hierarchy level 4: Folder "<instance id>"
+                // this prevents collisions for multiple instances under the same meshProject
+                name: service.instance.serviceInstanceId,
+                entries: [
+                  { name: `${service.instance.serviceInstanceId}.main.tf`, content: tf(azure_subscription_id, params.vNetRegion, params.count_of_leading_1_bits_in_the_routing_mask) },
+                ],
+              }
+            ],
+          },
+        ],
+      }],
+    };
+  }
+}
+
+function tf (azure_subscription_id, vNetRegion, count_of_leading_1_bits_in_the_routing_mask) {
+  return `provider "azurerm" {
+  features {}
+  tenant = "5f0e994b-6436-4f58-be96-4dc7bebff827" // Meshcloud GmbH AAD tenant
+  subscription = "${azure_subscription_id}"
+}
+resource "azurerm_resource_group" "infrastructure_rg" {
+  name     = "infrastructure_rg"
+  location = "West Europe"
+}
+// module "vnet" {
+//   source              = "Azure/vnet/azurerm"
+//   resource_group_name = azurerm_resource_group.infrastructure_rg.name
+//   address_space       = ["10.0.0.0/${count_of_leading_1_bits_in_the_routing_mask}"]
+//
+//   depends_on = [azurerm_resource_group.infrastructure_rg]
+// }`
+}
+
+const handlers = {
+  "49f7d4f4-a00b-483a-9cc9-2519f9a7c6da": new VnetHandler(),
+};
+
+handlers;
