0e8854a37fbdb430d31bc845dee13701e64934e0
VNet Bot by example.com <azure-platform-operators@example.com>
2021-06-08 12:18:18 +0000

    Updating VNets

--- network/likvid-mobile/develop/0685ddf4-1fe5-4c1f-9152-8edf74df5112/0685ddf4-1fe5-4c1f-9152-8edf74df5112.main.tf
+++ network/likvid-mobile/develop/0685ddf4-1fe5-4c1f-9152-8edf74df5112/0685ddf4-1fe5-4c1f-9152-8edf74df5112.main.tf
@@ -1,14 +1,1 @@
-provider "azurerm" {
-  features {}
-}
-resource "azurerm_resource_group" "infrastructure_rg" {
-  name     = "infrastructure_rg"
-  location = "West Europe"
-}
-// module "vnet" {
-//   source              = "Azure/vnet/azurerm"
-//   resource_group_name = azurerm_resource_group.infrastructure_rg.name
-//   address_space       = ["10.0.0.0/27"]
-//
-//   depends_on = [azurerm_resource_group.infrastructure_rg]
-// }
+#DESTROYED
