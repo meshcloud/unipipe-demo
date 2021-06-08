47f2edc51b6f1933f5bb10bcca188715a1bdc135
DorukAkinci <dakinci@meshcloud.io>
2021-06-08 10:50:17 +0200

    feat(handler): add a workaround for deleted instances

--- network/network-handlers.js
+++ network/network-handlers.js
@@ -6,6 +6,7 @@ class VnetHandler {
     const params = service.instance.parameters;
     const context = service.instance.context;
     const bindings = service.bindings;
+    const deleted = service.instance.deleted;
 
     return {
       // Hierarchy level 1
@@ -23,7 +24,7 @@ class VnetHandler {
                 // this prevents collisions for multiple instances under the same meshProject
                 name: service.instance.serviceInstanceId,
                 entries: [
-                  { name: `${service.instance.serviceInstanceId}.main.tf`, content: tf(params.vNetRegion, params.count_of_leading_1_bits_in_the_routing_mask) },
+                  { name: `${service.instance.serviceInstanceId}.main.tf`, content: deleted?`#DESTROYED`:tf(params.vNetRegion, params.count_of_leading_1_bits_in_the_routing_mask) },
                 ],
               }
             ],
