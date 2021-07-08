7ce2ae36007c832cb10bb1a883f10bdbae036f31
VNet Bot by example.com <azure-platform-operators@example.com>
2021-07-08 11:59:15 +0000

    Updating VNets

--- network/likvid-mobile/develop/0ceb44b4-4f73-407b-9055-57240a2eec2d/0ceb44b4-4f73-407b-9055-57240a2eec2d.main.tf
+++ network/likvid-mobile/develop/0ceb44b4-4f73-407b-9055-57240a2eec2d/0ceb44b4-4f73-407b-9055-57240a2eec2d.main.tf
@@ -1,1 +1,19 @@
-#DESTROYED
+#DELETED
+terraform {
+  backend "local" {
+  }
+}
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "infrastructure_rg" {
+  name     = "0ceb44b4-4f73-407b-9055-57240a2eec2d"
+  location = "GermanyWestCentral"
+}
+// module "vnet" {
+//   source              = "Azure/vnet/azurerm"
+//   resource_group_name = azurerm_resource_group.infrastructure_rg.name
+//   address_space       = ["10.0.0.0/27"]
+//
+//   depends_on = [azurerm_resource_group.infrastructure_rg]
+// }
