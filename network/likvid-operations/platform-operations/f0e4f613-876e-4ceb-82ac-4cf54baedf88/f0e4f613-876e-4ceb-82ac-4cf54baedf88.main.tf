3115fbb3b733611123021e555b6605dd23b8794d
VNet Bot by example.com <azure-platform-operators@example.com>
2021-09-23 11:38:11 +0000

    Updating VNets

---
+++ network/likvid-operations/platform-operations/f0e4f613-876e-4ceb-82ac-4cf54baedf88/f0e4f613-876e-4ceb-82ac-4cf54baedf88.main.tf
@@ -0,0 +1,54 @@
+
+terraform {
+  backend "local" {
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  address_space = "10.0.4.0/28"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "f0e4f613-876e-4ceb-82ac-4cf54baedf88"
+  location = "GermanyWestCentral"
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "UP-f0e4f613-876e-4ceb-82ac-4cf54baedf88"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = [ local.address_space ]
+
+  subnet {
+    name           = "public"
+    address_prefix = cidrsubnet(local.address_space, 1, 0)
+    security_group = azurerm_network_security_group.main.id
+  }
+
+  subnet {
+    name           = "private"
+    address_prefix = cidrsubnet(local.address_space, 1, 1)
+    security_group = azurerm_network_security_group.main.id
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "SG-f0e4f613-876e-4ceb-82ac-4cf54baedf88"
+  location            = "GermanyWestCentral"
+  resource_group_name = azurerm_resource_group.main.name
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
