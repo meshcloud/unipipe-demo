31533e9f681bce95f73de594655ce6a847587693
VNet Bot by example.com <azure-platform-operators@example.com>
2022-03-23 19:41:48 +0000

    Updating GitHub Services

---
+++ github-service/likvid-operations/platform-operations/4260b696-f4b3-4f64-95a4-e622dab184ad/4260b696-f4b3-4f64-95a4-e622dab184ad.main.tf
@@ -0,0 +1,29 @@
+
+terraform {
+  backend "local" {
+  }
+
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "4.22.0"
+    }
+  }
+}
+
+provider "github" {
+  # Token is provided by GitHub action runner environment variable GITHUB_TOKEN
+  owner = "meshcloud"
+}
+
+resource "github_repository" "example" {
+  name        = "number-four"
+  description = "Infrastructure repository for project platform-operations of customer likvid-operations."
+
+  visibility = "private"
+
+  template {
+    owner      = "meshcloud"
+    repository = "unipipe-demo-infrastructure-template"
+  }
+}
