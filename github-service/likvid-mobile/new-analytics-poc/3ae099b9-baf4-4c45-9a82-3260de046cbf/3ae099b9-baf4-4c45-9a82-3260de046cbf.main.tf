7f42ed659117067ecdd9e29669e0bb2cdebd4c09
VNet Bot by example.com <azure-platform-operators@example.com>
2022-03-24 08:05:52 +0000

    Updating VNets

--- github-service/likvid-mobile/new-analytics-poc/3ae099b9-baf4-4c45-9a82-3260de046cbf/3ae099b9-baf4-4c45-9a82-3260de046cbf.main.tf
+++ github-service/likvid-mobile/new-analytics-poc/3ae099b9-baf4-4c45-9a82-3260de046cbf/3ae099b9-baf4-4c45-9a82-3260de046cbf.main.tf
@@ -36,3 +36,40 @@ resource "github_repository" "example" {
     repository = "unipipe-demo-infrastructure-template"
   }
 }
+
+module "github_actions_sa" {
+  source  = "terraform-google-modules/service-accounts/google"
+  version = "~> 4.0"
+
+  project_id = "likvid-mobile-new-analyt-u41"
+
+  names        = ["githubactionssa"]
+  display_name = "Github Actions SA"
+
+  project_roles = ["likvid-mobile-new-analyt-u41=>roles/editor"]
+}
+
+module "gh_oidc" {
+  source         = "terraform-google-modules/github-actions-runners/google//modules/gh-oidc"
+  project_id     = "likvid-mobile-new-analyt-u41"
+  pool_id        = "github-identity-pool"
+  provider_id    = "github-identity-provider"
+  sa_mapping     = {
+    "my_service_account" = {
+      sa_name   = "projects/likvid-mobile-new-analyt-u41/serviceAccounts/${module.github_actions_sa.email}"
+      attribute = "attribute.repository/meshcloud/number-seven"
+    }
+  }
+}
+
+resource "github_actions_secret" "gcp_workload_identity_provider" {
+  repository       = github_repository.example.name
+  secret_name      = "GCP_WORKLOAD_IDENTITY_PROVIDER"
+  plaintext_value  = module.gh_oidc.provider_name
+}
+
+resource "github_actions_secret" "gcp_service_account" {
+  repository       = github_repository.example.name
+  secret_name      = "GCP_SERVICE_ACCOUNT"
+  plaintext_value  = module.github_actions_sa.email
+}
